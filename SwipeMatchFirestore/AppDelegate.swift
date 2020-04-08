8739b6e08cd81f4c47f6e003d6fb9c23943c43b1
Mohammed Hamdi <mhah000@hotmail.com>
2020-04-08 17:25:11 +0200

    - Add pods to the project. - Add user registration functionality.

--- SwipeMatchFirestore/AppDelegate.swift
+++ SwipeMatchFirestore/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if #available(iOS 13.0, *) {
             
         } else {
+            FirebaseApp.configure()
+            
             window?.makeKeyAndVisible()
             window?.rootViewController = RegistrationController()
         }
--- SwipeMatchFirestore/Controllers/RegistrationController.swift
+++ SwipeMatchFirestore/Controllers/RegistrationController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import JGProgressHUD
 
 class RegistrationController: UIViewController {
 
@@ -56,6 +58,7 @@ class RegistrationController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.heightAnchor.constraint(equalToConstant: 44).isActive = true
         button.layer.cornerRadius = 22
+        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
         return button
     }()
     
@@ -179,5 +182,30 @@ class RegistrationController: UIViewController {
         } else {
             registrationViewModel.password = textField.text
         }
+    }
+    
+    @objc fileprivate func handleRegister() {
+        print("Register user")
+        self.handleTapDismiss()
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("Error registering user:", error)
+                self.showHUDWithError(error: error)
+                return
+            }
+            
+            print("Successfully registered user:", result?.user.uid ?? "")
+        }
+    }
+    
+    fileprivate func showHUDWithError(error: Error) {
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Failed registeration"
+        hud.detailTextLabel.text = error.localizedDescription
+        hud.show(in: self.view)
+        hud.dismiss(afterDelay: 4)
     }
 }
