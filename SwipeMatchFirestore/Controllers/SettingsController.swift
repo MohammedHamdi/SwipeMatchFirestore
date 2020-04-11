02c951eeba63f485c5215a0b7377dbdea2f1ba63
Mohammed Hamdi <mhah000@hotmail.com>
2020-04-11 19:03:43 +0200

    - Save user photos to firestore.

--- SwipeMatchFirestore/Controllers/SettingsController.swift
+++ SwipeMatchFirestore/Controllers/SettingsController.swift
@@ -31,6 +31,44 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         let imageButton = (picker as? CustomImagePickerController)?.imageButton
         imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
         dismiss(animated: true)
+        
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        guard let uploadData = selectedImage?.jpegData(compressionQuality: 0.75) else { return }
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Uploading image..."
+        hud.show(in: view)
+        
+        ref.putData(uploadData, metadata: nil) { (_, error) in
+            
+            if let error = error {
+                hud.dismiss()
+                print("Failed to upload image to storage:", error)
+                return
+            }
+            
+            print("Finished uploading image")
+            ref.downloadURL { (url, error) in
+                hud.dismiss()
+                
+                if let error = error {
+                    print("Failed to retrieve download URL:", error)
+                    return
+                }
+                
+                print("Finished getting download url:", url?.absoluteString ?? "")
+                
+                if imageButton == self.image1Button {
+                    self.user?.imageUrl1 = url?.absoluteString
+                } else if imageButton == self.image2Button {
+                    self.user?.imageUrl2 = url?.absoluteString
+                } else {
+                    self.user?.imageUrl3 = url?.absoluteString
+                }
+                
+            }
+        }
     }
     
     func createButton(selector: Selector) -> UIButton {
@@ -193,11 +231,21 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     }
     
     fileprivate func loadUserPhotos() {
-        guard let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) else { return }
-        SDWebImageManager.shared.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
-            self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        if let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
         }
-        
+        if let imageUrl = user?.imageUrl2, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image2Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
+        }
+        if let imageUrl = user?.imageUrl3, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image3Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
+        }
     }
     
     @objc fileprivate func handleSave() {
@@ -206,6 +254,8 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             "uid": uid,
             "fullName": user?.name ?? "",
             "imageUrl1": user?.imageUrl1 ?? "",
+            "imageUrl2": user?.imageUrl2 ?? "",
+            "imageUrl3": user?.imageUrl3 ?? "",
             "age": user?.age ?? -1,
             "profession": user?.profession ?? ""
         ]
@@ -219,6 +269,10 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             
             if let error = error {
                 print("Failed to save user settings:", error)
+                hud.textLabel.text = "Failed to save data"
+                hud.detailTextLabel.text = error.localizedDescription
+                hud.show(in: self.view)
+                hud.dismiss(afterDelay: 5, animated: true)
                 return
             }
             
--- SwipeMatchFirestore/Model/User.swift
+++ SwipeMatchFirestore/Model/User.swift
@@ -15,13 +15,17 @@ struct User: ProducesCardViewModel {
     var profession: String?
 //    let imageNames: [String]
     var imageUrl1: String?
+    var imageUrl2: String?
+    var imageUrl3: String?
     var uid: String?
     
     init(dictionary: [String: Any]) {
         self.name = dictionary["fullName"] as? String ?? ""
         self.age = dictionary["age"] as? Int
         self.profession = dictionary["profession"] as? String
-        self.imageUrl1 = dictionary["imageUrl1"] as? String ?? ""
+        self.imageUrl1 = dictionary["imageUrl1"] as? String
+        self.imageUrl2 = dictionary["imageUrl2"] as? String
+        self.imageUrl3 = dictionary["imageUrl3"] as? String
         self.uid = dictionary["uid"] as? String ?? ""
     }
     
@@ -34,6 +38,11 @@ struct User: ProducesCardViewModel {
         let professionString = profession != nil ? profession! : "Not available"
         attributedText.append(NSAttributedString(string: "\n\(professionString)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
         
-        return CardViewModel(imageNames: [imageUrl1 ?? ""], attributedString: attributedText, textAlignment: .left)
+        var imageUrls = [String]()
+        if let url = imageUrl1 { imageUrls.append(url) }
+        if let url = imageUrl2 { imageUrls.append(url) }
+        if let url = imageUrl3 { imageUrls.append(url) }
+        
+        return CardViewModel(imageNames: imageUrls, attributedString: attributedText, textAlignment: .left)
     }
 }
