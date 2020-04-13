45f4aac245ac9b074125b36f9ebe9f6e272a71d7
Mohammed Hamdi <mhah000@hotmail.com>
2020-04-13 13:32:28 +0200

    - Add age range sliders.

--- SwipeMatchFirestore/Controllers/SettingsController.swift
+++ SwipeMatchFirestore/Controllers/SettingsController.swift
@@ -150,8 +150,10 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             headerLabel.text = "Profession"
         case 3:
             headerLabel.text = "Age"
-        default:
+        case 4:
             headerLabel.text = "Bio"
+        default:
+            headerLabel.text = "Seeking Age Range"
         }
         return headerLabel
     }
@@ -166,7 +168,7 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+        return 6
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -175,7 +177,24 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     
     // MARK:- Table view delegate
     
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Age Range Cell
+        if indexPath.section == 5 {
+            let ageRangeCell = AgeRangeCell(style: .default, reuseIdentifier: nil)
+            ageRangeCell.minSlider.addTarget(self, action: #selector(handleMinAgeChange), for: .valueChanged)
+            ageRangeCell.maxSlider.addTarget(self, action: #selector(handleMaxAgeChange), for: .valueChanged)
+            
+            ageRangeCell.minLabel.text = "Min \(user?.minSeekingAge ?? -1)"
+            ageRangeCell.minSlider.value = Float(user?.minSeekingAge ?? 18)
+            ageRangeCell.maxLabel.text = "Max \(user?.maxSeekingAge ?? -1)"
+            ageRangeCell.maxSlider.value = Float(user?.maxSeekingAge ?? 18)
+            return ageRangeCell
+        }
+        
         let cell = SettingsCell(style: .default, reuseIdentifier: nil)
         
         switch indexPath.section {
@@ -209,6 +228,30 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     
     @objc func handleAgeChange(textField: UITextField) {
         user?.age = Int(textField.text ?? "")
+    }
+    
+    @objc fileprivate func handleMinAgeChange(slider: UISlider) {
+        let indexPath = IndexPath(row: 0, section: 5)
+        let ageRangeCell = tableView.cellForRow(at: indexPath) as! AgeRangeCell
+        if slider.value >= ageRangeCell.maxSlider.value {
+            ageRangeCell.maxSlider.value = slider.value
+        }
+        ageRangeCell.minLabel.text = "Min \(Int(slider.value))"
+        ageRangeCell.maxLabel.text = "Max \(Int(ageRangeCell.maxSlider.value))"
+        
+        self.user?.minSeekingAge = Int(slider.value)
+    }
+    
+    @objc fileprivate func handleMaxAgeChange(slider: UISlider) {
+        let indexPath = IndexPath(row: 0, section: 5)
+        let ageRangeCell = tableView.cellForRow(at: indexPath) as! AgeRangeCell
+        if slider.value <= ageRangeCell.minSlider.value {
+            ageRangeCell.minSlider.value = slider.value
+        }
+        ageRangeCell.maxLabel.text = "Max \(Int(slider.value))"
+        ageRangeCell.minLabel.text = "Min \(Int(ageRangeCell.minSlider.value))"
+        
+        self.user?.maxSeekingAge = Int(slider.value)
     }
     
     // MARK:- Firebase functions
@@ -257,7 +300,9 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             "imageUrl2": user?.imageUrl2 ?? "",
             "imageUrl3": user?.imageUrl3 ?? "",
             "age": user?.age ?? -1,
-            "profession": user?.profession ?? ""
+            "profession": user?.profession ?? "",
+            "minSeekingAge": user?.minSeekingAge ?? -1,
+            "maxSeekingAge": user?.maxSeekingAge ?? -1
         ]
         
         let hud = JGProgressHUD(style: .dark)
---
+++ SwipeMatchFirestore/Views/AgeRangeCell.swift
@@ -0,0 +1,62 @@
+//
+//  AgeRangeCell.swift
+//  SwipeMatchFirestore
+//
+//  Created by Mohammed Hamdi on 4/13/20.
+//  Copyright © 2020 Mohammed Hamdi. All rights reserved.
+//
+
+import UIKit
+
+class AgeRangeCell: UITableViewCell {
+
+    let minSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 18
+        slider.maximumValue = 100
+        return slider
+    }()
+    
+    let maxSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 18
+        slider.maximumValue = 100
+        return slider
+    }()
+    
+    let minLabel: AgeRangeLabel = {
+        let label = AgeRangeLabel()
+        label.text = "Min 29"
+        return label
+    }()
+    
+    let maxLabel: AgeRangeLabel = {
+        let label = AgeRangeLabel()
+        label.text = "Max 56"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let overallStackView = UIStackView(arrangedSubviews: [
+            UIStackView(arrangedSubviews: [minLabel, minSlider]),
+            UIStackView(arrangedSubviews: [maxLabel, maxSlider])
+        ])
+        overallStackView.axis = .vertical
+        overallStackView.spacing = 16
+        
+        addSubview(overallStackView)
+        overallStackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class AgeRangeLabel: UILabel {
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 80, height: 0)
+        }
+    }
+}
