1fbc86a3f0feec7a8aee3bd41676630f7a0afd94
Mohammed Hamdi <mhah000@hotmail.com>
2020-04-24 14:25:50 +0200

    - Commit MatchesHorizontalController

---
+++ SwipeMatchFirestore/Controllers/MatchesMessages/MatchesHorizontalController.swift
@@ -0,0 +1,59 @@
+//
+//  MatchesHorizontalController.swift
+//  SwipeMatchFirestore
+//
+//  Created by Mohammed Hamdi on 4/24/20.
+//  Copyright © 2020 Mohammed Hamdi. All rights reserved.
+//
+
+import LBTATools
+import Firebase
+
+class MatchesHorizontalController: LBTAListController<MatchCell, Match>, UICollectionViewDelegateFlowLayout {
+    
+    var rootMatchesController: MatchesMessagesController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        
+        fetchMatches()
+    }
+    
+    fileprivate func fetchMatches() {
+        guard let currentUserId = Auth.auth().currentUser?.uid else { return }
+        
+        Firestore.firestore().collection("matches_messages").document(currentUserId).collection("matches").getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Failed to fetch matches:", error)
+                return
+            }
+            
+            var matches = [Match]()
+            snapshot?.documents.forEach({ (documentSnapshot) in
+                let dictionary = documentSnapshot.data()
+                matches.append(.init(dictionary: dictionary))
+            })
+            
+            self.items = matches
+            self.collectionView.reloadData()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 110, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 4, bottom: 0, right: 16)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let match = self.items[indexPath.item]
+        
+        rootMatchesController?.didSelectMatchFromHeader(match: match)
+    }
+}
